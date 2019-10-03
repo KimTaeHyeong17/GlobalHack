ec8426b7bf74225262903ceee0abca100fa29af2
TaeHyeongKim <uuzaza@naver.com>
2019-10-03 12:14:36 +0900

    android OCR using google vision implemeted

--- app/src/main/java/com/example/globalhackapp/Fragment/FirstFragment.java
+++ app/src/main/java/com/example/globalhackapp/Fragment/FirstFragment.java
@@ -1,31 +1,152 @@
 package com.example.globalhackapp.Fragment;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Context;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
+import android.util.SparseArray;
 import android.view.LayoutInflater;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.globalhackapp.R;
+import com.google.android.gms.vision.CameraSource;
+import com.google.android.gms.vision.Detector;
+import com.google.android.gms.vision.text.TextBlock;
+import com.google.android.gms.vision.text.TextRecognizer;
+
+import java.io.IOException;
 
 
 public class FirstFragment extends Fragment {
+
+    private View v;
+
+    SurfaceView cameraView;
+    TextView textView;
+    CameraSource cameraSource;
+    final int RequestCamearPermissionID = 1001;
+
+
 
     public FirstFragment() {
         // Required empty public constructor
     }
 
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case RequestCamearPermissionID:
+            {
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                {
+                    if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED)
+                    {
+                        return;
+                    }
+                    try {
+                        cameraSource.start(cameraView.getHolder());
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        v = inflater.inflate(R.layout.fragment_first, container, false);
+
+        bindUI();
+        SetTextRecognizer();
+        return v;
+
+    }
+
+    private void bindUI(){
+        cameraView = v.findViewById(R.id.surface_view);
+        textView = v.findViewById(R.id.tv);
+    }
+
+    private void SetTextRecognizer(){
+        TextRecognizer textRecognizer = new TextRecognizer.Builder(getContext()).build();
+        if(!textRecognizer.isOperational()){
+            Log.w("FirstFragment","Detector dependencies are not yet available");
+        }else{
+            cameraSource = new CameraSource.Builder(getContext(),textRecognizer)
+                    .setFacing(CameraSource.CAMERA_FACING_BACK)
+                    .setRequestedPreviewSize(1280,1024)
+                    .setRequestedFps(2.0f)
+                    .setAutoFocusEnabled(true)
+                    .build();
+        }
+        cameraView.getHolder().addCallback(new SurfaceHolder.Callback() {
+            @Override
+            public void surfaceCreated(SurfaceHolder holder) {
+                try{
+                    if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions(getActivity(),new String[]{Manifest.permission.CAMERA},
+                                RequestCamearPermissionID);
+                        return;
+                    }
+                    cameraSource.start(cameraView.getHolder());
+                }catch (IOException e){
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+
+            }
+
+            @Override
+            public void surfaceDestroyed(SurfaceHolder holder) {
+                cameraSource.stop();
+            }
+        });
+
+        textRecognizer.setProcessor(new Detector.Processor<TextBlock>() {
+            @Override
+            public void release() {
+
+            }
+
+            @Override
+            public void receiveDetections(Detector.Detections<TextBlock> detections) {
+                final SparseArray<TextBlock> items = detections.getDetectedItems();
+                if(items.size() != 0 ){
+                    textView.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            StringBuilder stringBuilder = new StringBuilder();
+                            for (int i = 0 ; i < items.size() ; i++){
+                                TextBlock item = items.valueAt(i);
+                                stringBuilder.append(item.getValue());
+                                stringBuilder.append("\n");
+                            }
+                            textView.setText(stringBuilder.toString());
+                        }
+                    });
+                }
+            }
+        });
     }
 
 
